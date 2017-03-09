ab5ddac9234ed39a22f1544862001c4f68ce7b7e
zhansong <114837616@qq.com>
2017-03-09 14:12:51 +0800

    1

---
+++ src/main/java/com/ks3/cdn/EurekaClient.java
@@ -0,0 +1,26 @@
+package com.ks3.cdn;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by ZHANSONG on 2017/3/8.
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+@EnableAutoConfiguration
+public class EurekaClient {
+    @RequestMapping("/")
+    public String home() {
+        return "Hello world";
+    }
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(EurekaClient.class).web(true).run(args);
+    }
+}
